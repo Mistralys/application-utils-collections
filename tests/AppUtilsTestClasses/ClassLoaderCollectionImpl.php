8b289d6af61426f71f369b329294a78538015b75
Mistralys <s.mordziol@gmail.com>
2025-04-15 08:42:48 +0200

    Added tests.

---
+++ tests/AppUtilsTestClasses/ClassLoaderCollectionImpl.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package App Utils Tests
+ * @subpackage Collections
+ */
+
+declare(strict_types=1);
+
+namespace AppUtilsTestClasses;
+
+use AppUtils\ClassHelper;
+use AppUtils\Collections\BaseClassLoaderCollection;
+use AppUtils\FileHelper\FolderInfo;
+use AppUtilsTestClasses\StringClassesFolder\StringItemC;
+
+/**
+ * @package App Utils Tests
+ * @subpackage Collections
+ *
+ * @method BaseStringItem[] getAll()
+ * @method BaseStringItem getByID(string $id)
+ * @method BaseStringItem getDefault()
+ */
+class ClassLoaderCollectionImpl extends BaseClassLoaderCollection
+{
+    public function getDefaultID(): string
+    {
+        return StringItemC::ITEM_ID;
+    }
+
+    public function getClassesFolder(): FolderInfo
+    {
+        return FolderInfo::factory(__DIR__.'/StringClassesFolder');
+    }
+
+    public function getInstanceOfClassName(): ?string
+    {
+        return null;
+    }
+
+    protected function createItemInstance(string $class): BaseStringItem
+    {
+        return ClassHelper::requireObjectInstanceOf(
+            BaseStringItem::class,
+            new $class()
+        );
+    }
+}
