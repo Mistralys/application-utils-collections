1ada4ba3f44bf91a71c9c9ab22348c169c779a7d
Mistralys <s.mordziol@gmail.com>
2025-04-18 14:46:53 +0200

    Added PHPdocs.

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -1,17 +1,29 @@
 <?php
+/**
+ * @package AppUtils
+ * @subpackage Composer
+ */
 
 declare(strict_types=1);
 
 namespace AppUtils;
 
+/**
+ * Used as a repository for composer scripts.
+ *
+ * @package AppUtils
+ * @subpackage Composer
+ */
 class ComposerScripts
 {
     public static function clearClassCache() : void
     {
+        echo "Clearing class cache..." . PHP_EOL;
+
         require_once __DIR__ . '/../tests/bootstrap.php';
 
         ClassHelper::getRepositoryManager()->clearCache();
 
-        echo 'Class cache cleared.' . PHP_EOL;
+        echo 'DONE.' . PHP_EOL;
     }
 }
