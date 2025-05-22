818f88686c985c1b01ea683877b0ca8bb4cb29c7
Mistralys <s.mordziol@gmail.com>
2025-05-22 07:54:55 +0200

    Added PHPDocs.

--- src/Collections/CollectionException.php
+++ src/Collections/CollectionException.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @package App Utils
+ * @subpackage Collections
+ */
 
 declare(strict_types=1);
 
@@ -6,6 +10,12 @@ namespace AppUtils\Collections;
 
 use AppUtils\BaseException;
 
+/**
+ * Exception class for the Collections module.
+ *
+ * @package App Utils
+ * @subpackage Collections
+ */
 class CollectionException extends BaseException
 {
     public const ERROR_CLASS_CACHE_FOLDER_NOT_SET = 176901;
