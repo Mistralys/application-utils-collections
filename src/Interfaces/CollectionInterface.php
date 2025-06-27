f94a5131f5efa5ddcb8d8dffe330c3f6b4e84bdd
Mistralys <s.mordziol@gmail.com>
2025-06-27 16:08:41 +0200

    Removed unused import.

--- src/Interfaces/CollectionInterface.php
+++ src/Interfaces/CollectionInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace AppUtils\Interfaces;
 
-use AppUtils\Collections\Events\ItemsInitializedEvent;
-
 /**
  * Interface for classes that can be used as collections.
  *
