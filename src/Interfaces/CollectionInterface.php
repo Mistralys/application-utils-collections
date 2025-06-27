b8a376abe7a873617f517b5e8fe18cc286a83d84
Mistralys <s.mordziol@gmail.com>
2025-06-27 08:27:42 +0200

    Added event handling methods.

--- src/Interfaces/CollectionInterface.php
+++ src/Interfaces/CollectionInterface.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace AppUtils\Interfaces;
 
+use AppUtils\Collections\Events\ItemsInitializedEvent;
+
 /**
  * Interface for classes that can be used as collections.
  *
- * NOTE: This base interface does not contain all relevant
- * methods, these are added by the type-specific interfaces,
- * like {@see StringPrimaryCollectionInterface}.
+ * > NOTE: This base interface does not contain all relevant
+ * > methods, these are added by the type-specific interfaces,
+ * > like {@see StringPrimaryCollectionInterface}.
  *
  * @package App Utils
  * @subpackage Collections
@@ -30,4 +32,39 @@ interface CollectionInterface
     public function getDefault() : CollectionRecordInterface;
 
     public function countRecords() : int;
+
+    /**
+     * Adds a listener that will be called when the items
+     * in the collection have been fully initialized.
+     *
+     * The listener gets a single argument, which is the
+     * event object of the type {@see ItemsInitializedEvent}.
+     *
+     * @param callable $listener
+     * @return int Number of the listener, which can be used to remove it later. NOTE: The number is unique across all collections during the application lifetime.
+     */
+    public function onItemsInitialized(callable $listener) : int;
+
+    /**
+     * Removes a listener by its number.
+     *
+     * @param int $listenerNumber
+     * @return $this
+     */
+    public function removeListener(int $listenerNumber) : self;
+
+    /**
+     * Removes all listeners for all events.
+     *
+     * @return $this
+     */
+    public function removeAllListeners() : self;
+
+    /**
+     * Returns the listeners for a specific event.
+     *
+     * @param string $eventName
+     * @return array<int,callable> Listener numbers as keys, listeners as values.
+     */
+    public function getEventListeners(string $eventName) : array;
 }
